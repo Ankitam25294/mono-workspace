f562676965e37abf78bfb31a9b715a91c25cb59f
unknown <ankitamedhekar8@gmail.com>
2024-08-28 22:21:31 +0530

    first commit

---
+++ projects/mfe-app/src/app/todo-list/todo-list.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { TodoListComponent } from './todo-list.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forChild([{
+      path: '',
+      component: TodoListComponent
+    }])
+  ]
+})
+export class TodoListModule { }
